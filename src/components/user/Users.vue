56d4fbc67386e1b7a62b2cd513271b294c2dd17b
胡怀行 <550134146@qq.com>
2020-11-24 17:00:15 +0800

    新增用户列表

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,8 +1,102 @@
 <template>
-	<div>User-list</div>
+	<div>
+		<el-breadcrumb separator="/">
+			<el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+			<el-breadcrumb-item><a href="/">用户管理</a></el-breadcrumb-item>
+			<el-breadcrumb-item>用户列表</el-breadcrumb-item>
+		</el-breadcrumb>
+		<el-card class="box-card">
+			<el-row :gutter="20">
+				<el-col :span="8">
+					<el-input placeholder="请输入内容" class="input-with-select" v-model="queryInfo.query" clearable @clear="getUserList">
+						<el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
+					</el-input>
+				</el-col>
+				<el-col :span="4">
+					<el-button type="primary">添加用户</el-button>
+				</el-col>
+			</el-row>
+			<!-- 列表数据 -->
+			<el-table :data="userlist" border stripe>
+				<el-table-column type="index"></el-table-column>
+				<el-table-column label="姓名" prop="username"></el-table-column>
+				<el-table-column label="邮箱" prop="email"></el-table-column>
+				<el-table-column label="电话" prop="mobile"></el-table-column>
+				<el-table-column label="角色" prop="role_name"></el-table-column>
+				<el-table-column label="状态">
+					<template slot-scope="scope">
+						<el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"></el-switch>
+					</template>
+				</el-table-column>
+				<el-table-column label="操作" >
+					<template slot-scope="scope">
+						<el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+						<el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+							<el-button type="primary" icon="el-icon-setting" size="mini"></el-button>
+						</el-tooltip>
+						<el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+					</template>
+				</el-table-column>
+			</el-table>
+			<!-- 分页 -->
+			<el-pagination
+		      @size-change="handleSizeChange"
+		      @current-change="handleCurrentChange"
+		      :current-page="queryInfo.pagenum"
+		      :page-sizes="[1, 2, 5, 10]"
+		      :page-size="queryInfo.pagesize"
+		      layout="total, sizes, prev, pager, next, jumper"
+		      :total="total">
+		    </el-pagination>
+		</el-card>
+	</div>
 </template>
 <script>
-	export default {}
+export default {
+	data() {
+		return {
+			queryInfo: {
+				query: '',
+				pagenum: 1,
+				pagesize: 2
+			},
+			userlist: [],
+			total: 0
+		}
+	},
+	created() {
+		this.getUserList()
+	},
+	methods: {
+		async getUserList () {
+			const { data: res } = await this.$http.get('users', { 
+				params: this.queryInfo
+			})
+			if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+			this.userlist = res.data.users
+			this.total = res.data.total
+		},
+		// 监听pagesize变化
+		handleSizeChange (newSize) {
+			this.queryInfo.pagesize = newSize
+			this.getUserList()
+		}, 
+		// 监听页码值 改变事件
+		handleCurrentChange (newPage) {
+			this.queryInfo.pagenum = newPage
+			this.getUserList()
+		},
+		async userStateChanged (userinfo) {
+			console.log(userinfo)
+			const {data: res} = await this.$http.put(`users/${userinfo.id}/state/${userinfo.mg_state}`)
+			if (res.meta.status !== 200) {
+				userinfo.msg_state = !userinfo.msg_state
+				this.$message.error('更新用户状态失败！')
+			}
+			this.$message.success('更新成功。')
+		}
+	}
+}
 </script>
 
 <style lang="less" scope>
