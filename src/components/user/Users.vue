12f3ae16267cfeaf95d73563b41125fe3d19ebcb
huhuaihang <huhuaihangf@163.com>
2020-11-25 17:06:57 +0800

    完成用户列表功能开发

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -34,7 +34,7 @@
 						<el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
 							<el-button type="primary" icon="el-icon-setting" size="mini"></el-button>
 						</el-tooltip>
-						<el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+						<el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)"></el-button>
 					</template>
 				</el-table-column>
 			</el-table>
@@ -246,8 +246,28 @@ export default {
 				this.editDialogVisible = false
 				this.getUserList()
 				this.$message.success('更新成功。')
-			})
-			
+			})	
+		},
+		// 删除用户
+		async removeUserById(id) {
+			const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+	          confirmButtonText: '确定',
+	          cancelButtonText: '取消',
+	          type: 'warning'
+	        }).catch(error =>{
+	        	return error
+	        })
+	        
+	        if (confirmResult !== 'confirm') {
+	        	return this.$message.info('已经取消删除。')
+	        }
+
+	        const {data: res} = await this.$http.delete('users/' + id)
+	        if (res.meta.status !== 200) {
+	        	return this.$message.error('删除失败！')
+	        }
+	        this.$message.success('删除成功。')
+	        this.getUserList()
 		}
 	}
 }
